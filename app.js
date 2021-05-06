62e900e6914d378cccc5554f2c0f0f012cee4513
danielmolnar <daniel-molnar@web.de>
2021-05-06 13:14:50 +0200

    added styling for todo items, added functions to add and remove todo list items

---
+++ app.js
@@ -0,0 +1,51 @@
+// selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+//Event Listeners
+todoButton.addEventListener('click', addToDo);
+todoList.addEventListener('click', deleteCheck);
+
+//Functions
+
+function addToDo(event) {
+  event.preventDefault();
+
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  const newTodo = document.createElement('li');
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  trashButton.classList.add('trash-btn');
+  todoDiv.appendChild(trashButton);
+
+  todoList.appendChild(todoDiv);
+  todoInput.value = '';
+}
+
+function deleteCheck(event) {
+  const item = event.target;
+
+  if (item.classList[0] === 'trash-btn') {
+    const todo = item.parentElement;
+    todo.classList.add('fall');
+    todo.addEventListener('transitionend', function () {
+      todo.remove();
+    });
+  }
+
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+  }
+}
